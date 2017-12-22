adc411c72f9e6fb5d5cce7427e752aeaf4cbced5
Alrikozord <Alrikozord@gmail.com>
2017-12-22 15:00:54 +0100

    stats are editable now

--- src/app/swn/character-sheet/stats/stats.component.ts
+++ src/app/swn/character-sheet/stats/stats.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Character } from "../../models/index";
+import { EditToggleBaseComponent } from "../../../basic-components/edit-toggle-components/index";
 
 @Component({
   selector: "app-stats",
   templateUrl: "./stats.component.html",
   styleUrls: ["./stats.component.css"]
 })
-export class StatsComponent implements OnInit {
+export class StatsComponent extends EditToggleBaseComponent implements OnInit {
   @Input() character: Character;
 
-  constructor() {}
+  constructor() {
+    super();
+  }
 
   ngOnInit() {}
 }
