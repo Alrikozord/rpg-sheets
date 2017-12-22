b1bca36074409e994e71c156b58bfb4e81ebc3c3
Alrikozord <Alrikozord@gmail.com>
2017-12-22 12:57:16 +0100

    added modifiable list to armor

--- src/app/swn/character-sheet/armors/armor/armor.component.ts
+++ src/app/swn/character-sheet/armors/armor/armor.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Armor } from '../../../models/index';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit, Input } from "@angular/core";
+import { Armor } from "../../../models/index";
+import { FormsModule } from "@angular/forms";
+import { EditToggleBaseComponent } from "../../../../basic-components/edit-toggle-components/index";
 
 @Component({
-  selector: 'app-armor',
-  templateUrl: './armor.component.html',
-  styleUrls: ['./armor.component.css']
+  selector: "app-armor",
+  templateUrl: "./armor.component.html",
+  styleUrls: ["./armor.component.css"]
 })
-export class ArmorComponent implements OnInit {
-
+export class ArmorComponent extends EditToggleBaseComponent implements OnInit {
   @Input() armor: Armor;
   @Input() index: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    super();
   }
 
+  ngOnInit() {}
 }
--- src/app/swn/character-sheet/armors/armors.component.ts
+++ src/app/swn/character-sheet/armors/armors.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Character } from "../../models/index";
+import { Component, OnInit, Input } from "@angular/core";
+import { Character, Armor } from "../../models/index";
+import { EditToggleListBase } from "../../../basic-components/edit-toggle-components/index";
 
 @Component({
-  selector: 'app-armors',
-  templateUrl: './armors.component.html',
-  styleUrls: ['./armors.component.css']
+  selector: "app-armors",
+  templateUrl: "./armors.component.html",
+  styleUrls: ["./armors.component.css"]
 })
-export class ArmorsComponent implements OnInit {
-
+export class ArmorsComponent extends EditToggleListBase implements OnInit {
   @Input() character: Character;
-  
-  constructor() { }
 
-  ngOnInit() {
+  constructor() {
+    super();
   }
 
+  ngOnInit() {}
+
+  public onAdd(): void {
+    this.character.armor.push(new Armor());
+  }
+  public onRemove(index: number): void {
+    this.character.armor.slice(index, 1);
+  }
 }
