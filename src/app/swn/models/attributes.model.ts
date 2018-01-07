418956a799e245f909c4991abadebe5d92dc1a39
Alrikozord <Alrikozord@gmail.com>
2018-01-07 12:42:33 +0100

    added initialization to serializable props. removed demo character data from CharacterSheet.

--- src/app/swn/models/attributes.model.ts
+++ src/app/swn/models/attributes.model.ts
@@ -7,4 +7,17 @@ export class Attributes {
   int: Attribute;
   str: Attribute;
   wis: Attribute;
+
+  constructor() {
+    this.initialize();
+  }
+
+  private initialize(): void {
+    this.cha = new Attribute(10);
+    this.con = new Attribute(10);
+    this.dex = new Attribute(10);
+    this.int = new Attribute(10);
+    this.str = new Attribute(10);
+    this.wis = new Attribute(10);
+  }
 }
--- src/app/swn/models/serializable-properties.class.ts
+++ src/app/swn/models/serializable-properties.class.ts
@@ -1,24 +1,59 @@
-import { Attributes, Focus, Skill, Armor, Weapon, Cyberware, Goal, CharacterClass } from "./index";
+import {
+  Attributes,
+  Focus,
+  Skill,
+  Armor,
+  Weapon,
+  Cyberware,
+  Goal,
+  CharacterClass
+} from "./index";
 
 export class SerializableProperties {
-    public name: string;
-    public class: CharacterClass;
-    public species: string;
-    public background: string;
-    public homeworld: string;
-    public xp: number;
-    public stats: Attributes;
-    public hp: { current: number, max: number };
-    public systemstrain: { current: number, max: number };
-    public effort: { scene: number, day: number, other: number };
-    public foci: Focus[];
-    public skills: Skill[];
-    public techniques: any[];
-    public finances: { credits: number, items: any[] };
-    public armor: Armor[];
-    public weapons: Weapon[];
-    public equipment: any[];
-    public cyberware: Cyberware[];
-    public goals: Goal[];
-    public notes: string;
+  public name: string;
+  public class: CharacterClass;
+  public species: string;
+  public background: string;
+  public homeworld: string;
+  public xp: number;
+  public stats: Attributes = new Attributes();
+  public hp: { current: number; max: number };
+  public systemstrain: { current: number; max: number };
+  public effort: { scene: number; day: number; other: number };
+  public foci: Focus[];
+  public skills: Skill[];
+  public techniques: any[];
+  public finances: { credits: number; items: any[] };
+  public armor: Armor[];
+  public weapons: Weapon[];
+  public equipment: any[];
+  public cyberware: Cyberware[];
+  public goals: Goal[];
+  public notes: string;
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize(): void {
+    this.name = "character name";
+    this.class = CharacterClass.warrior;
+    this.species = "human";
+    this.background = "background";
+    this.homeworld = "homeworld";
+    this.xp = 0;
+    this.stats = new Attributes();
+    this.hp = { current: 0, max: 0 };
+    this.systemstrain = { current: 0, max: 0 };
+    this.effort = { scene: 0, day: 0, other: 0 };
+    this.foci = new Array<Focus>();
+    this.skills = new Array<Skill>();
+    this.techniques = new Array<any>();
+    this.finances = { credits: 0, items: new Array<any>() };
+    this.armor = new Array<Armor>();
+    this.weapons = new Array<Weapon>();
+    this.equipment = new Array<any>();
+    this.cyberware = new Array<Cyberware>();
+    this.goals = new Array<Goal>();
+  }
 }
