97af23a402f49b41ceebf5fc3d0d66b852a52c61
Alrikozord <Alrikozord@gmail.com>
2018-01-12 01:17:01 +0100

    now filtering psychic skills from selection if char is not a psycher

--- src/app/swn/data-files/skill-data-service.ts
+++ src/app/swn/data-files/skill-data-service.ts
@@ -2,6 +2,7 @@ import { DataServiceBase } from "./data-service-base";
 import { HttpClient } from "@angular/common/http";
 import { Skill } from "../models/index";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class SkillDataService extends DataServiceBase<Skill> {
@@ -16,5 +17,28 @@ export class SkillDataService extends DataServiceBase<Skill> {
     instance.type = fileRepresentation.type;
     instance.details = fileRepresentation.details.toString();
     return instance;
+  }
+
+  getFilteredNames(
+    term: string,
+    includePsychics: boolean = true
+  ): Observable<string[]> {
+    return this.getNames(includePsychics).map(item =>
+      item.filter(v => {
+        return v.toLowerCase().indexOf(term.toLowerCase()) > -1;
+      })
+    );
+  }
+
+  getNames(includePsychics: boolean = true): Observable<string[]> {
+    if (includePsychics) {
+      return super.getNames();
+    } else {
+      return this.getAll().map(elements =>
+        elements
+          .filter(skill => skill.type !== "psychic")
+          .map(item => item.name)
+      );
+    }
   }
 }
